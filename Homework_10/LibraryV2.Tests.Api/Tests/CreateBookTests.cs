25dbe55074a0fcb5bdb97f6c647ef67b46fb8674
Sergii Zvonov <s.zvonov@gmail.com>
2024-08-15 18:54:26 +0300

    Library test WIP

--- Homework_10/LibraryV2.Tests.Api/Tests/CreateBookTests.cs
+++ Homework_10/LibraryV2.Tests.Api/Tests/CreateBookTests.cs
@@ -5,15 +5,33 @@ namespace LibraryV2.Tests.Api.Tests;
 
 public class CreateBookTests : LibraryV2TestFixture
 {
-    private LibraryHttpService _libraryHttpService;
-    
+    private Book _book;
+
+    public async Task CreateBook(string title, string author, int year)
+    {
+        _book = new Book()
+        {
+            Title = title,
+            Author = author,
+            YearOfRelease = year
+        };
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(response, Is.Not.Null);
+            Assert.That(response.Title, Is.EqualTo(_book.Title));
+            Assert.That(response.Author, Is.EqualTo(_book.Author));
+            Assert.That(response.YearOfRelease, Is.EqualTo(_book.YearOfRelease));
+
+        });
+    }
     [SetUp]
     public new void SetUp()
     {
         _libraryHttpService = new LibraryHttpService();
         _libraryHttpService.Configure("http://localhost:5111/");
     }
-    
+
     //TODO cover with tests all endpoints from Books controller
     // Create book
 }
--- Homework_10/LibraryV2.Tests.Api/Tests/GetBooksTests.cs
+++ Homework_10/LibraryV2.Tests.Api/Tests/GetBooksTests.cs
@@ -1,3 +1,4 @@
+using LibraryV2.Models;
 using LibraryV2.Tests.Api.Fixtures;
 using LibraryV2.Tests.Api.Services;
 
@@ -6,6 +7,20 @@ namespace LibraryV2.Tests.Api.Tests;
 [TestFixture]
 public class GetBooksTests : LibraryV2TestFixture
 {
+    [Test]
+    public async Task GetBooksByTitle()
+    {
+        var book = _books.First();
+        List<Book> response = await _libraryHttpService.GetBooksByTitle(book.Title);
+
+        Assert.Multiple( () => 
+        {
+            Assert.That(response, Is.Not.Null);
+            Assert.That(response[0].Title, Is.EqualTo(book.Title));
+            Assert.That(response[0].Author, Is.EqualTo(book.Author));
+            Assert.That(response[0].YearOfRelease, Is.EqualTo(book.YearOfRelease));
+        });
+    }
     private LibraryHttpService _libraryHttpService;
 
     [SetUp]
