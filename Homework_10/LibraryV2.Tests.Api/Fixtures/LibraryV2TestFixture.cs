4b91b27e7aa060399477a7a066261dfc826759d8
Sergii Zvonov <s.zvonov@gmail.com>
2024-08-15 18:54:26 +0300

    Library test WIP

--- Homework_10/LibraryV2.Tests.Api/Fixtures/LibraryV2TestFixture.cs
+++ Homework_10/LibraryV2.Tests.Api/Fixtures/LibraryV2TestFixture.cs
@@ -1,3 +1,7 @@
+<<<<<<< HEAD
+=======
+using LibraryV2.Models;
+>>>>>>> 25dbe55 (Library test WIP)
 using LibraryV2.Tests.Api.Services;
 
 namespace LibraryV2.Tests.Api.Fixtures;
@@ -5,10 +9,44 @@ namespace LibraryV2.Tests.Api.Fixtures;
 [TestFixture]
 public class LibraryV2TestFixture : GlobalSetUpFixture
 {
+<<<<<<< HEAD
     protected LibraryHttpService LibraryHttpService2;
+=======
+    internal readonly LibraryHttpService _libraryHttpService = new();
+    internal readonly Dictionary<User, string> _users = new();
+    internal readonly List<Book> _books = new();
+
+>>>>>>> 25dbe55 (Library test WIP)
     [OneTimeSetUp]
-    public void SetUp()
+    public async Task SetUp()
     {
+        _libraryHttpService.Configure("http://localhost:5111");
+        for (int i = 0; i < 10; i++)
+        {
+            User user = new()
+            {
+                FullName = Guid.NewGuid() + "-n" + i,
+                Password = Guid.NewGuid() + "-n" + i,
+                NickName = Guid.NewGuid() + "-n" + i
+            };
+
+            await _libraryHttpService.CreateUser(user);
+            var token = await _libraryHttpService.LogIn(user);
+            _users.Add(user, token.Token.Trim('"'));
+        }
+
+        for (int i = 0; i < 10; i++)
+        {
+            Book book = new()
+            {
+                Title = Guid.NewGuid() + "-n" + i,
+                Author = Guid.NewGuid() + "-n" + i,
+                YearOfRelease = 0000 + i
+            };
+            _books.Add(book);
+            var token = _users.First().Value;
+            await _libraryHttpService.CreateBook(token, book);            
+        }
     }
 
     [OneTimeTearDown]
--- Homework_10/LibraryV2.Tests.Api/Tests/GetBooksTests.cs
+++ Homework_10/LibraryV2.Tests.Api/Tests/GetBooksTests.cs
@@ -1,3 +1,4 @@
+using LibraryV2.Models;
 using LibraryV2.Tests.Api.Fixtures;
 using LibraryV2.Tests.Api.Services;
 
@@ -6,12 +7,33 @@ namespace LibraryV2.Tests.Api.Tests;
 [TestFixture]
 public class GetBooksTests : LibraryV2TestFixture
 {
-    private LibraryHttpService _libraryHttpService;
-
-    [SetUp]
-    public void SetUp()
+    [Test]
+    public async Task GetBooksByTitle()
     {
-        _libraryHttpService = new LibraryHttpService();
-        _libraryHttpService.Configure("http://localhost:5111/");
+        var book = _books.First();
+        List<Book> response = await _libraryHttpService.GetBooksByTitle(book.Title);
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(response, Is.Not.Null);
+            Assert.That(response[0].Title, Is.EqualTo(book.Title));
+            Assert.That(response[0].Author, Is.EqualTo(book.Author));
+            Assert.That(response[0].YearOfRelease, Is.EqualTo(book.YearOfRelease));
+        });
+    }
+
+    [Test]
+    public async Task GetBooksByAuthor()
+    {
+        var book = _books.Last();
+        List<Book> response = await _libraryHttpService.GetBooksByAuthor(book.Author);
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(response, Is.Not.Null);
+            Assert.That(response[0].Title, Is.EqualTo(book.Title));
+            Assert.That(response[0].Author, Is.EqualTo(book.Author));
+            Assert.That(response[0].YearOfRelease, Is.EqualTo(book.YearOfRelease));
+        });
     }
 }
