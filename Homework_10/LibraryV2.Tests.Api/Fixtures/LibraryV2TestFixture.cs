516011dc4067ae9c3a37983b758212e84fe8e859
Sergii Zvonov <s.zvonov@gmail.com>
2024-08-17 20:20:29 +0300

    Resolve merge conflict

--- Homework_10/LibraryV2.Tests.Api/Fixtures/LibraryV2TestFixture.cs
+++ Homework_10/LibraryV2.Tests.Api/Fixtures/LibraryV2TestFixture.cs
@@ -6,40 +6,9 @@ namespace LibraryV2.Tests.Api.Fixtures;
 [TestFixture]
 public class LibraryV2TestFixture : GlobalSetUpFixture
 {
-    internal readonly LibraryHttpService _libraryHttpService = new();
-    internal readonly Dictionary<User, string> _users = new();
-    internal readonly List<Book> _books = new();
-
     [OneTimeSetUp]
-    public async Task SetUp()
+    public void SetUp()
     {
-        _libraryHttpService.Configure("http://localhost:5111");
-        for (int i = 0; i < 10; i++)
-        {
-            User user = new()
-            {
-                FullName = Guid.NewGuid() + "-n" + i,
-                Password = Guid.NewGuid() + "-n" + i,
-                NickName = Guid.NewGuid() + "-n" + i
-            };
-
-            await _libraryHttpService.CreateUser(user);
-            var token = await _libraryHttpService.LogIn(user);
-            _users.Add(user, token.Token.Trim('"'));
-        }
-
-        for (int i = 0; i < 10; i++)
-        {
-            Book book = new()
-            {
-                Title = Guid.NewGuid() + "-n" + i,
-                Author = Guid.NewGuid() + "-n" + i,
-                YearOfRelease = 0000 + i
-            };
-            _books.Add(book);
-            var token = _users.First().Value;
-            await _libraryHttpService.CreateBook(token, book);            
-        }
     }
 
     [OneTimeTearDown]
